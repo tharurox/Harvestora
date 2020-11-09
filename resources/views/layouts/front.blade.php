2bfde600746f53610cfcafded9504df206f42827
Tharaka Ranathunga <tharakaravishan@gmail.com>
2020-11-09 19:53:48 +0530

    latest code

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Harvestora Forum </title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.min.css">
+
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous">
+    </script>
+
+ </head>
+<body>
+
+<div class="navbar">
+    <a class = "navbar-brand" href = "#"> Harvestora </a>
+    <ul class = "nav navbar-nav">
+        <li class = "active">
+
+        <a href="#"> Home </a>
+        </li>
+        <a href="#"> Link</a>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
+
+
