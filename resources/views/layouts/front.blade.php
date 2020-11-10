adc3af4d1f069e1ba58da9cda8d719c0f4ae801c
Udara Madhushan <udaram188@gmail.com>
2020-11-10 11:35:46 +0530

    ui modifications

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,7 +7,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Harvestora Forum </title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/darkly/bootstrap.min.css" integrity="sha384-nNK9n28pDUDDgIiIqZ/MiyO3F4/9vsMtReZK39klb/MtkZI3/LtjSjlmyVPS3KdN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.min.css">
@@ -18,16 +18,34 @@
  </head>
 <body>
 
-<div class="navbar navbar-inverse">
-    <a class = "navbar-brand" href = "#"> Harvestora </a>
-    <ul class = "nav navbar-nav">
-        <li class = "active">
-            <a href="#"> Home </a>
-        </li>
-            <a href="#"> Link</a>
-        </li>
-    </ul>
-</div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="display-1 " href="/">Harvestora</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse justify-content-center row " id="navbarColor02">
+         
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/home">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/thread">Threads</a>
+            </li>
+          </ul>
+       
+
+       
+          <form class="form-inline my-2 my-lg-0 justify-content-end">
+            <input class="form-control mr-sm-2 " type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+       
+      </nav>
 
 <div class ="container">
     @yield('content')
@@ -35,7 +53,7 @@
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous">
 </script>
-
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'></script>
 </body>
 </html>
 
