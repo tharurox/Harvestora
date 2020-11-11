9423357825831dc59e57ebf8b4b908f714b17d90
Ovini <ovinivisna39@gmail.com>
2020-11-11 18:15:25 +0530

    authentication & authorisation

---
+++ resources/views/layouts/partials/categories.blade.php
@@ -0,0 +1,19 @@
+<div class="col-md-3">
+
+    <h4>Catogory</h4>
+
+    <ul class="list-group">
+
+        <a href="{{route('thread.index')}}" class="list-group-item">
+        
+        <span class="badge">14</span>
+        All thread
+        </a>
+
+        <a href="" class="list-group-item">
+
+            <span class="badge">2</span>
+           PHP
+        </a>
+    </ul>
+  </div>
