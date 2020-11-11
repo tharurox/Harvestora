83657d3ac2d0a231ae75cc17b665ac87520c262a
Udara Madhushan <udaram188@gmail.com>
2020-11-11 12:36:00 +0530

    thread crud modifications

---
+++ resources/views/thread/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.front')
+
+@section('heading','Create Thread')
+@section('content')
+
+@include('layouts.partials.error')
+@include('layouts.partials.success')
+
+<div class="row">
+
+        <div class="well">
+
+                <form class='form-vertical' action="{{route('thread.store')}}" method='post' role='form' id='create-thread-form'>
+                 {{csrf_field()}}
+
+                 <div class="form-group">
+                    <label for="subject">Subject</label>
+                 <input type="text" class="form-control" name='subject' id=''  placeholder='Input...' value="{{old('subject')}}">
+                 </div>
+
+
+                 <div class="form-group">
+
+                <label for="subject"> Type</label>
+                 <input type="text" class="form-control" name='type' id=''   placeholder='input....' value='{{old('type')}}'>
+
+                 </div>
+                
+                
+                 <div class="form-group">
+
+                    <label for="thread"> Thread</label>
+                     <textarea type="text" class="form-control" name='thread' id=''   placeholder='input....' value='{{old('thread')}}'> </textarea>
+    
+                     </div>
+                
+                
+    
+                
+                     <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+        </div>
+
+</div>
+
+@endsection
