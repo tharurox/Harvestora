41d418c1f770c422dde82e8c01b80a0ccdcb407a
Ovini <ovinivisna39@gmail.com>
2020-11-11 18:15:25 +0530

    authentication & authorisation

--- resources/views/thread/single.blade.php
+++ resources/views/thread/single.blade.php
@@ -11,18 +11,13 @@
 
         {{$thread->thread}}
 
-
-
     </div>
     <br>
 
+@if(auth()->user()->id == $thread->user_id)
     <div class="actions">
-
-
         
     <a href="{{route('thread.edit',$thread->id)}}" class="btn btn-info btn-xs">Edit</a>
-
-
 
     <form action="{{route('thread.destroy',$thread->id)}}"  method='POST' class="inline-it">
     
@@ -33,4 +28,5 @@
     
     </form>
     </div>
+@endif
 @endsection
