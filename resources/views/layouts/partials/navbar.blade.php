ea86e544ee20b98544f236d14295e17592d3bcf5
Ovini <ovinivisna39@gmail.com>
2020-11-18 17:58:39 +0530

    user profile update

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -41,6 +41,16 @@
                               </a>
 
                               <a href="{{ route('logout') }}"
+                                  onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                                  Logout
+                              </a>
+
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                  {{ csrf_field() }}
+                              </form>
+                          </li>
+                      </ul>
                   </li>
               @endif
           </ul>
