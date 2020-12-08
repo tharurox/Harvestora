c03f2e1eddcbaeda54a99bf756a6464499162cc5
uranat1 <tharaka.ranathunga@pearson.com>
2020-12-08 14:27:04 +0530

    ui changes

--- resources/views/layouts/partials/categories.blade.php
+++ resources/views/layouts/partials/categories.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-3">
 
-    <a class="btn btn-info"  href="{{route('thread.create')}}">Create Thread</a> <br><br>
+    <a class="btn btn-primary"  href="{{route('thread.create')}}">Create Thread</a> <br><br>
     <h4>Tags</h4>
 
     <ul class="list-group">
