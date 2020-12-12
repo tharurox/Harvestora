79ac3d7d0b648b4b73811c607012f9606299ca23
Udara Madhushan <udaram188@gmail.com>
2020-12-12 16:59:00 +0530

    Ui changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,6 @@
 
 @section('banner')
     
-<<<<<<< HEAD
-<section class="jumbotron  bg-light text-dark">
- 
-    <div class="container ">
-       
-      <div class="card text-center">
-        
-        <div class="card-body">
-          <h5 class="card-title display-3">Welcome to Harvestora Community</h5>
-          <p class="card-text display-5">An online Agricultural community to Help each other</p>
-          @if (Auth::guest())
-          <a href="{{ route('register') }}" class="btn btn-primary btn-lg m-1">Join Our community</a>
-          @endif
-          <a href="{{route('thread.create')}}" class="btn btn-success btn-lg m-1">Start a Thread</a>
-        </div>
-       
-      </div>
-=======
 <div class="jumbotron">
     <div class="container">
         <h1> Join Harvesotra Community </h1>
@@ -28,7 +10,6 @@
 
         <p>
         <a class="btn btn-primary btn-lg"  href="{{route('home.learnmore')}}"> Learn More.! </a>
->>>>>>> 2c9557ac6dac256afa482da16861c1ed1ff58437
         </p>
     </div>
   </section>
